70526576e78e5fa0820679fe52ce3db19fb9636f
Shashwat Sinha <shashwatsinha2702@gmail.com>
2019-06-07 01:00:47 +0530

    Created deployment build and added library for serving file

---
+++ build/precache-manifest.c767b5e879248d805055dbe75199cf04.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b6d923b14fd51df43b6f492b4671c570",
+    "url": "/index.html"
+  },
+  {
+    "revision": "20665de2dd0b15486f35",
+    "url": "/static/css/main.6cb620f9.chunk.css"
+  },
+  {
+    "revision": "575aa3a46f00bed19b93",
+    "url": "/static/js/2.d1306dc4.chunk.js"
+  },
+  {
+    "revision": "20665de2dd0b15486f35",
+    "url": "/static/js/main.290eab41.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "c078507b7b2f6ef1d4c5c5e619d31433",
+    "url": "/static/media/hamburger.c078507b.svg"
+  }
+]);
