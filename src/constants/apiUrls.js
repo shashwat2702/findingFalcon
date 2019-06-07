15a7521a6d8f9dc502ad4a0beaa0b4868a7659fe
Shashwat Sinha <shashwatsinha2702@gmail.com>
2019-06-07 19:42:38 +0530

    Updated URLs to be HTTPS instead of HTTP

--- src/constants/apiUrls.js
+++ src/constants/apiUrls.js
@@ -1,4 +1,4 @@
 export const getPlanet = 'https://findfalcone.herokuapp.com/planets';
 export const getVehicles = 'https://findfalcone.herokuapp.com/vehicles';
-export const getToken = 'http://findfalcone.herokuapp.com/token';
-export const findFalcon = 'http://findfalcone.herokuapp.com/find';
+export const getToken = 'https://findfalcone.herokuapp.com/token';
+export const findFalcon = 'https://findfalcone.herokuapp.com/find';
